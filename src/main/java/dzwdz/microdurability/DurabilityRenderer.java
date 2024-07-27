f96b674fb1d18c2f0e402644d7f81869ae3e615f
Derek Lee <derek.scott.lee13@gmail.com>
2024-07-27 02:41:29 -0700

    Update to 1.21

--- src/main/java/dzwdz/microdurability/DurabilityRenderer.java
+++ src/main/java/dzwdz/microdurability/DurabilityRenderer.java
@@ -4,13 +4,14 @@ import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.ColorHelper.Argb;
 
 public class DurabilityRenderer implements HudRenderCallback {
 
-    private static final Identifier TEX = new Identifier("microdurability", "textures/gui/icons.png");
+    private static final Identifier TEX = Identifier.of("microdurability", "textures/gui/icons.png");
     private static final int MASK = Argb.getArgb(0xFF, 0x00, 0x00, 0x00);
     private final MinecraftClient client;
 
@@ -21,18 +22,18 @@ public class DurabilityRenderer implements HudRenderCallback {
     }
 
     @Override
-    public void onHudRender(DrawContext drawContext, float tickDelta) {
+    public void onHudRender(DrawContext drawContext, RenderTickCounter tickCounter) {
         if (!client.interactionManager.hasStatusBars())
             return;
 
         final int scaledWidth = client.getWindow().getScaledWidth();
         final int scaledHeight = client.getWindow().getScaledHeight();
-        time = (time + tickDelta) % (EntryPoint.CONFIG.blinkTime * 40f);
+        time = (time + tickCounter.getTickDelta(false)) % (EntryPoint.CONFIG.blinkTime * 40f);
 
         // render the held item warning
         if (EntryPoint.CONFIG.toolWarning) {
             for (ItemStack stack : client.player.getHandItems()) {
-                if (EntryPoint.shouldWarn(stack)) {
+                if (EntryPoint.shouldWarn(stack, client.world)) {
                     // todo: this doesn't align with the crosshair at some resolutions
                     drawContext.drawTexture(TEX, scaledWidth / 2 - 2, scaledHeight / 2 - 18, 0, 0, 3, 11);
                     break;
@@ -43,7 +44,7 @@ public class DurabilityRenderer implements HudRenderCallback {
         // render the armor durability
         if (EntryPoint.CONFIG.blinkTime > 0) {
             for (ItemStack stack : client.player.getArmorItems()) {
-                if (time < EntryPoint.CONFIG.blinkTime * 20f && EntryPoint.shouldWarn(stack)) {
+                if (time < EntryPoint.CONFIG.blinkTime * 20f && EntryPoint.shouldWarn(stack, client.world)) {
                     return;
                 }
             }
--- src/main/java/dzwdz/microdurability/EntryPoint.java
+++ src/main/java/dzwdz/microdurability/EntryPoint.java
@@ -6,6 +6,8 @@ import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.enchantment.Enchantments;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.world.World;
 
 public class EntryPoint implements ClientModInitializer {
 
@@ -19,11 +21,16 @@ public class EntryPoint implements ClientModInitializer {
         DurabilityRenderer.EVENT.register(new DurabilityRenderer());
     }
 
-    public static boolean shouldWarn(ItemStack stack) {
+    public static boolean shouldWarn(final ItemStack stack, final World world) {
         if (stack == null || !stack.isDamageable())
             return false;
-        if (CONFIG.requireMending && EnchantmentHelper.getLevel(Enchantments.MENDING, stack) <= 0)
-            return false;
+        if (CONFIG.requireMending) {
+            final var mending = world.getRegistryManager().getWrapperOrThrow(RegistryKeys.ENCHANTMENT)
+                    .getOptional(Enchantments.MENDING);
+            if (mending.isPresent() && EnchantmentHelper.getLevel(mending.get(), stack) <= 0) {
+                return false;
+            }
+        }
         int durability = stack.getMaxDamage() - stack.getDamage();
         return durability < CONFIG.minDurability
                 && durability * 100f / CONFIG.minPercent < stack.getMaxDamage();
